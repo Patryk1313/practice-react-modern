f68b2b047dc36a42557771d2f11dd19e4534417a
Patryk <patryk.13@o2.pl>
2024-06-20 19:08:17 +0200

    Done 01 & 02 tasks

--- 02/components/App.js
+++ 02/components/App.js
@@ -1,17 +1,18 @@
-// ./src/components/App.js
 import React from 'react';
 import Box from './Box';
+import TextContext from '../context/context';
 
 class App extends React.Component {
     state = {
-        text: 'React HelloWorld Modern!',
+        render() {
+            const { text } = this.state;
+            const { Provider } = TextContext;
+
+            return (
+                <Provider value={text}>
+                    <Box />
+                </Provider>
+            );
+        },
     };
-
-    render() {
-        const { text } = this.state;
-
-        return <Box text={text} />;
-    }
 }
-
-export default App;
